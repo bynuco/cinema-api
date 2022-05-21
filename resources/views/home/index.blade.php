f326561f3403c740e6c36ca4001bc9efc441da4d
bynuco <nesimiztrk@gmail.com>
2022-05-21 13:03:16 +0300

    Project Initial

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master_layout')
+
+@section('content')
+    <div style="height:100%; display: flex; justify-content: center; align-items: center">
+        <h1>Cinema API Project</h1>
+    </div>
+@endsection
